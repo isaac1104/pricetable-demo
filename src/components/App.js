19e56530346a89ec22130dff9a520b50f2fbf414
Isaac Kwon <isaac@virtualgraffiti.com>
2018-02-22 17:36:56 -0800

    init

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Navbar from "./Navbar";
+import Content from "./Content";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <Content/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <div>
+      <img src="/images/1.jpg"/>
+      <h6>Meraki MR33 Access Point 802.11ac Wave 2 2x2 MR33-HW</h6>
+    </div>
+  );
+}
+
+export default Content;
